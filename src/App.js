226e96477382ab0cc6802849e75e4e9b73744f57
“Sagar_Pachode3” <sagarpachode3@gmail.com>
2024-03-19 17:05:19 +0530

    Scroll indicator logic. scroll.css UPDATE.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LightDarkModeSwitch from "./components/light-dark-mode";
 import LoadMoreData from "./components/load-more-data";
 import QRCodeGenrator from "./components/qr-generator";
 import RandomColor from "./components/random-color";
+import ScrollIndicator from "./components/scroll-indicator";
 import StarRating from "./components/star-rating";
 import TreeView from "./components/tree-view";
 import menus from "./components/tree-view/data";
@@ -38,7 +39,10 @@ function App() {
       {/* <QRCodeGenrator /> */}
 
       {/* Switch between light and Dark mode */}
-      <LightDarkModeSwitch/>
+      {/* <LightDarkModeSwitch/> */}
+
+      {/* Scroll - Indicator */}
+      <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} />
     </div>
   );
 }
